43f2ea11f6ba4f7a475e5aed983cdb832e303fae
Nicolás Herrera <josnikh@gmail.com>
2022-06-30 16:11:47 -0500

    Creation of rest client

---
+++ src/helpers/api.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: 'http://localhost:8080/apiv1/',
+    timeout: 4000,
+    headers: {'apiKey': '1d68089ac25a6bfe801546e561339e0c6d452b1e'}
+})
+
+const errorHandler = (error) => {
+    if (error.response){
+        return {
+            type: "response",
+            error: error.response
+        }
+    }else if (error.request){
+        return {
+            type: "request",
+            error: error.request
+        }
+    }else{
+        return {
+            type: "unknown",
+            error: error.message
+        }
+    }
+}
+
+export const clientMethod = {
+    post: async (path, payload) => {
+        try{
+            return await API.post(path, payload);
+        }catch(e){
+            return {
+                data: errorHandler(e),
+                error: true
+            }
+        }
+        
+    },
+    get: async (path) => {
+        try{
+            return await API.get(path);
+        }catch(e){
+            return {
+                data: errorHandler(e),
+                error: true
+            }
+        }
+    }
+}
